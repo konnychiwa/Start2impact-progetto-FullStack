4eaf76c50e3cf7c29bb8b25a66a55fbf558943a8
Angelo Konara <angelokonara04@gmail.com>
2025-02-05 16:18:44 +0100

    Added the user authentication feature, login and registration

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,9 +1,46 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import './LoginPopup.css';
 import { assets } from '../../assets/assets';
+import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios';
 
 const LoginPopup = ({ setShowLogin }) => {
+  const { url, setToken } = useContext(StoreContext);
+
   const [currentState, setCurrentState] = useState('Login');
+
+  const [data, setData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+
+  const onLogin = async (event) => {
+    event.preventDefault();
+
+    let newUrl = url;
+    if (currentState === 'Login') {
+      newUrl += '/api/user/login';
+    } else {
+      newUrl += '/api/user/register';
+    }
+
+    const response = await axios.post(newUrl, data);
+
+    if (response.data.success) {
+      setToken(response.data.token);
+      localStorage.setItem('token', response.data.token);
+      setShowLogin(false);
+    } else {
+      alert(response.data.message);
+    }
+  };
 
   useEffect(() => {
     document.body.style.overflow = 'hidden';
@@ -15,7 +52,7 @@ const LoginPopup = ({ setShowLogin }) => {
 
   return (
     <div className="login-popup">
-      <form action="" className="login-popup-container">
+      <form onSubmit={onLogin} action="" className="login-popup-container">
         <div className="login-popup-title">
           <h2>{currentState}</h2>
           <img
@@ -26,13 +63,34 @@ const LoginPopup = ({ setShowLogin }) => {
         </div>
         <div className="login-popup-inputs">
           {currentState === 'Login' ? null : (
-            <input type="text" placeholder="Il tuo nome" required />
+            <input
+              name="name"
+              onChange={onChangeHandler}
+              value={data.name}
+              type="text"
+              placeholder="Il tuo nome"
+              required
+            />
           )}
-          <input type="email" placeholder="La tua mail" required />
-          <input type="password" placeholder="Password" required />
+          <input
+            name="email"
+            onChange={onChangeHandler}
+            value={data.email}
+            type="email"
+            placeholder="La tua mail"
+            required
+          />
+          <input
+            name="password"
+            onChange={onChangeHandler}
+            value={data.password}
+            type="password"
+            placeholder="Password"
+            required
+          />
         </div>
-        <button>
-          {currentState === 'Iscriviti' ? 'Create Account' : 'Accedi'}
+        <button type="submit">
+          {currentState === 'Login' ? 'Accedi' : 'Crea Account'}
         </button>
         <div className="login-popup-condition">
           {currentState === 'Login' ? null : <input type="checkbox" required />}

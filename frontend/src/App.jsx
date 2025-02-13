291810a7c58d250655b24059e8b9d0b4b3d305fe
Angelo Konara <angelokonara04@gmail.com>
2025-02-13 18:54:19 +0100

    Small sentence fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
-import Home from './pages/home/Home';
+import Home from './pages/Home/Home';
 import Cart from './pages/Cart/Cart';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import Footer from './components/Footer/Footer';
